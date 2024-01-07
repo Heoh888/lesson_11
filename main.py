f03b0969cd907975b82a022421551fc4a221198c
Aleksey Khodakow <a.khodakow@gmail.com>
2024-01-07 06:30:00 +0300

    implement query statistics by hour

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from chunks import Chunk
 import openai
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 # Глобальная переменная для подсчета общего количества запросов
-total_requests = 0
+requests_per_hour = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # инициализация индексной базы
 chunk = Chunk(path_to_base="Simble.txt")
@@ -17,15 +19,24 @@ class Item(BaseModel):
 # создаем объект приложения
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
+
 @app.get("/api/get_number_requests")
 def get_number_requests():
-    global total_requests
-    return {"number_requests": total_requests}
+    global requests_per_hour
+    return requests_per_hour
 
 # функция обработки post запроса + декоратор
 @app.post("/api/get_answer")
 def get_answer(question: Item):
-    global total_requests
-    total_requests += 1
+    global requests_per_hour
+    current_hour = datetime.now().hour
+    requests_per_hour[current_hour] += 1
     answer = chunk.get_answer(query=question.text)
     return {"message": answer}
